7c94b04aca0636fa241d1d26fa066b9567ca8cbf
Luke Youngblood <luke@moonwell.fi>
2023-09-18 10:15:43 -0700

    Wrong timelock address

--- config/moonbase.config.ts
+++ config/moonbase.config.ts
@@ -3,7 +3,7 @@ import { Config } from './config-types'
 const config = new Config(
   '0x5713dd9a2b2ce515cf9232f48b457aebfb04b292',
   '0xa5B7D85a8f27dd7907dc8FdC21FA5657D5E2F901',
-  '0x3a9249d70dCb4A4E9ef4f3AF99a3A130452ec19B',
+  '0x43A720C2690B00Ae0a0F9E4b79ED24184D9e8F0A',
 )
 
 export default config
